00b277b37347437f836b063f73e8107de13c2d0c
XiongZhiBin <519083070@qq.com>
2023-03-01 01:04:50 +0800

    增加用于注册新插件的snippet。

---
+++ .config/nvim/lua/user/completion/snippets/lua.lua
@@ -0,0 +1,26 @@
+local alias = require("user.completion.snippets")
+
+local M = {}
+
+M.filetype = "lua"
+
+local function get_plugin_config_template()
+    return alias.s("plugin", {
+        alias.t({ "local untils = require(\"user.untils\")", "" }),
+        alias.t({ "", "M = {}", "" }),
+        alias.t({ "", "M.plugin_name = \"" }), alias.i(1), alias.t({ "\"", "" }),
+        alias.t({ "", "function M.setup(...)" }),
+        alias.t({ "", "    if untils.check_require(M.plugin_name) == false then" }),
+        alias.t({ "", "        return" }),
+        alias.t({ "", "    end", "", "" }),
+        alias.i(2),
+        alias.t({ "", "end", "" }),
+        alias.t({ "", "return M" }),
+    })
+end
+
+function M.setup(...)
+    alias.ls.add_snippets(M.filetype, { get_plugin_config_template(), })
+end
+
+return M
